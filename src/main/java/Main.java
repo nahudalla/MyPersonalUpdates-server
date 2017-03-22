d7b1be3d85e44e71c31155c5a072dd79f1870843
Nahuel Dallacamina <nahudalla@gmail.com>
2017-03-22 05:27:14 -0300

    Remove WebServer references.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,4 @@
-import com.mypersonalupdates.webserver.WebServer;
-
 public class Main {
     public static void main(String[] args) {
-        WebServer webServer = WebServer.getInstance();
-
-        webServer.init(8080);
     }
 }
