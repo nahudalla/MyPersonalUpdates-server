3e49fbe9e97e8b02a34b310dfad32872cc68cc1f
Noelia Fluxá <noefluxa@gmail.com>
2017-06-20 14:34:59 -0300

    Edición por Noe de UpdateProvidersManager. Cree la clase y implementé todos los métodos que están el diagrama de clases.

---
+++ src/main/java/com/mypersonalupdates/UpdatesProvidersManager.java
@@ -0,0 +1,32 @@
+package com.mypersonalupdates;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdatesProvidersManager {
+    private static UpdatesProvidersManager ourInstance = new UpdatesProvidersManager();
+
+    public static UpdatesProvidersManager getInstance() {
+        return ourInstance;
+    }
+
+    private UpdatesProvidersManager() {
+    }
+
+    private Map<Integer, UpdatesProvider> providers = new HashMap<>();
+    private UpdatesProvidersManager instance;
+
+    public Collection<UpdatesProvider> getProviders() {
+        return this.providers.values();
+    }
+
+    public UpdatesProvider getProvider(Integer ID){
+        return this.providers.get(ID);
+    }
+
+    public void addProvider(UpdatesProvider updatesProvider) {
+        this.providers.put(updatesProvider.getID(), updatesProvider);
+    }
+
+}
