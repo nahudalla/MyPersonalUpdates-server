bbab6633659b0c11c53d4a114982703fcf11d445
Nahuel Dallacamina <nahudalla@gmail.com>
2017-06-26 00:40:07 -0300

    UpdatesProvider class modified.
    Both subscribe and unsubscribe methods now take an instance of User as an extra argument.
    
    unsubscribe now returns true if the user was successfully unsubscribed from this provider with the given id.

--- src/main/java/com/mypersonalupdates/providers/UpdatesProvider.java
+++ src/main/java/com/mypersonalupdates/providers/UpdatesProvider.java
@@ -1,14 +1,16 @@
 package com.mypersonalupdates.providers;
 
+import com.mypersonalupdates.Filter;
 import com.mypersonalupdates.UpdatesConsumer;
+import com.mypersonalupdates.users.User;
+
+import java.util.List;
 
 public interface UpdatesProvider {
     Integer getID();
-    // TODO: actualizar signature
-    Integer subscribe (Filter filter, UpdatesConsumer cosumer);
-    // TODO: actualizar signature
-    void unsubscribe (Integer subscriberID);
-
-    // TODO: agregar: getName, getDescription, getAttributes, stop, equals
-    // TODO: agregar equals a diagrama
+    List<String> getAcceptedFilterFields();
+    // TODO: update diagram
+    Integer subscribe (User user, Filter filter, UpdatesConsumer cosumer);
+    // TODO: update diagram (true if the user was subscribed and now is not)
+    boolean unsubscribe (User user, Integer subscriberID);
 }
