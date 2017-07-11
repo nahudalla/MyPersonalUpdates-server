b6db7c91ec1217d6d705b44931fc52da3d164521
Nahuel Dallacamina <nahudalla@gmail.com>
2017-07-11 14:16:41 -0300

    Se agregaron TODOs para los cambios del diagrama.

--- src/main/java/com/mypersonalupdates/Update.java
+++ src/main/java/com/mypersonalupdates/Update.java
@@ -5,19 +5,7 @@ import java.util.List;
 
 public interface Update {
 
-    String getProviderID();
-    String getSource();
-    String getText();
-    String getID();
+    // TODO: arreglar nombre
     Date getTimestamps();
-
-
-    List<String> getLinks();
-    List<String> getMultimedia();
-    List<String> getHashtags();
-    List<String> getMentions();
-    Integer getLikesCount();
-    Integer getSharedCount();
-    boolean isOwnUpdate();
-    Update getOriginalUpdate();
+    // TODO: agregar: getProvider, getAttributeValues
 }
