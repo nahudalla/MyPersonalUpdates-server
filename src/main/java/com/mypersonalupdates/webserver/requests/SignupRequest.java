8843f44f95d5c981566bc17f749bede274fca63b
Nahuel Dallacamina <nahudalla@gmail.com>
2017-07-30 00:09:18 -0300

    Se eliminó la implementación anterior del servidor web.

--- src/main/java/com/mypersonalupdates/webserver/requests/SignupRequest.java
+++
@@ -1,5 +0,0 @@
-package com.mypersonalupdates.webserver.requests;
-
-public class SignupRequest extends LoginRequest{
-
-}
--- src/main/java/com/mypersonalupdates/webserver/session/SessionStore.java
+++
@@ -1,8 +0,0 @@
-package com.mypersonalupdates.webserver.session;
-
-public interface SessionStore {
-    String getNewSessionID();
-    void setSessionData(String id, String data);
-    String getSessionData(String id);
-    void destroySession(String id);
-}
